28afbb078267de2ba51493d921e79f1962416e29
tony <923940358@qq.com>
2024-10-03 19:49:23 +0800

    添加本地服务注册器，并在 provider 中注册服务

--- example-provider/src/main/java/com/downey/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/downey/example/provider/EasyProviderExample.java
@@ -1,10 +1,15 @@
 package com.downey.example.provider;
 
+import com.downey.example.common.service.UserService;
+import com.downey.mrpc.registry.LocalRegistry;
 import com.downey.mrpc.server.VertxHttpServer;
 
 public class EasyProviderExample {
 
     public static void main(String[] args) {
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+
         // 启动 web 服务
         VertxHttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
