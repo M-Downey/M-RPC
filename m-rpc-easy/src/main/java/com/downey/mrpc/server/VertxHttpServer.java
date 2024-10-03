796f5e9ceea693bfd5f08ce965e43029b26d86a2
tony <923940358@qq.com>
2024-10-03 20:37:04 +0800

    实现请求处理器（处理请求，拿到本地服务，调用，返回响应），在 Web 服务器中绑定该处理器

--- m-rpc-easy/src/main/java/com/downey/mrpc/server/VertxHttpServer.java
+++ m-rpc-easy/src/main/java/com/downey/mrpc/server/VertxHttpServer.java
@@ -12,14 +12,15 @@ public class VertxHttpServer implements HttpServer{
         io.vertx.core.http.HttpServer server = vertx.createHttpServer();
 
         // 监听端口并处理请求
-        server.requestHandler(httpServerRequest -> {
-            // 处理 http 请求
-            System.out.println("Received request: " + httpServerRequest.method() + " " + httpServerRequest.uri());
-            // 发送 http 响应
-            httpServerRequest.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Hello from Vert.x HTTP server!");
-        });
+//        server.requestHandler(httpServerRequest -> {
+//            // 处理 http 请求
+//            System.out.println("Received request: " + httpServerRequest.method() + " " + httpServerRequest.uri());
+//            // 发送 http 响应
+//            httpServerRequest.response()
+//                    .putHeader("content-type", "text/plain")
+//                    .end("Hello from Vert.x HTTP server!");
+//        });
+        server.requestHandler(new HttpServerHandler());
 
         // 启动 http 服务器并监听指定端口
         server.listen(port, httpServerAsyncResult -> {
