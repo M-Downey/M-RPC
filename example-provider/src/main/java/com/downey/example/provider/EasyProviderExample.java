0cca788616f1101524b878047ea967ee10292cf4
tony <923940358@qq.com>
2024-10-03 17:47:58 +0800

    provider 添加 web 服务器

--- example-provider/src/main/java/com/downey/example/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/downey/example/provider/EasyProviderExample.java
@@ -1,8 +1,12 @@
 package com.downey.example.provider;
 
+import com.downey.mrpc.server.VertxHttpServer;
+
 public class EasyProviderExample {
 
     public static void main(String[] args) {
-        // 提供服务
+        // 启动 web 服务
+        VertxHttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080);
     }
 }
